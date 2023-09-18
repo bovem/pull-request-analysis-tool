c5939d94b82b16a7025e241f6dad8ecc380a62c6
bovem <av_nish@outlook.com>
2023-09-18 16:53:09 +0530

    Added safe key reading from dict to avoid failures on open PRs

--- app/cleaner.py
+++ app/cleaner.py
@@ -1,8 +1,6 @@
 import json
 import os
-from utils import load_config, write_to_file, read_file
-
-config = load_config(os.environ.get("PAT_CONFIG_FILE"))
+from utils import write_to_file, read_file
 
 def clean_pr_data(config, pr_data_file):
     pr_data = read_file(pr_data_file)
@@ -11,31 +9,32 @@ def clean_pr_data(config, pr_data_file):
     for pr in pr_data:
         pr_labels = [x["name"] for x in pr["labels"]]
         cleaned_pr_data.append({
-            "PR URL": pr["html_url"],
-            "PR ID": pr["id"],
-            "PR Number": pr["number"],
-            "PR State": pr["state"],
-            "PR Title": pr["title"],
-            "PR Creator": pr["user"]["login"],
-            "PR Forked Repsitory Name": pr["head"]["repo"]["full_name"],
-            "PR Forked Repsitory URL": pr["head"]["repo"]["html_url"],
-            "PR Creator Type": pr["user"]["type"],
-            "PR Creation Time": pr["created_at"],
-            "PR Updation Time": pr["updated_at"],
-            "PR Closing Time": pr["closed_at"],
-            "PR Merging Time": pr["merged_at"],
-            "PR Merging Commit": pr["merge_commit_sha"],
+            "PR URL": pr.get("html_url", None),
+            "PR ID": pr.get("id", None),
+            "PR Number": pr.get("number", None),
+            "PR State": pr.get("state", None),
+            "PR Title": pr.get("title", None),
+            "PR Creator": pr.get("user", {}).get("login", None),
+            "PR Forked Repsitory Name": pr.get("head", {}).get("repo", {}).get("full_name", None),
+            "PR Forked Repsitory URL": pr.get("head", {}).get("repo", {}).get("html_url", None),
+            "PR Creator Type": pr.get('user', {}).get('type', None),
+            "PR Creation Time": pr.get("created_at", None),
+            "PR Updation Time": pr.get("updated_at", None),
+            "PR Closing Time": pr.get("closed_at", None),
+            "PR Merging Time": pr.get("merged_at", None),
+            "PR Merging Commit": pr.get("merge_commit_sha", None),
             "PR Labels": pr_labels,
-            "PR Comments URL": pr["comments_url"],
-            "PR Merge Branch From": pr["head"]["label"],
-            "PR Merge Brach To": pr["base"]["label"],
-            "PR Author Association": pr["author_association"]
+            "PR Comments URL": pr.get("comments_url", None),
+            "PR Merge Branch From": pr.get("head", {}).get("label", None),
+            "PR Merge Brach To": pr.get("base", {}).get("label"),
+            "PR Author Association": pr.get("author_association", None)
         }) 
     
-    write_to_file(cleaned_pr_data, 
-                  config["CLEANED_DATA_PATH"], 
-                  "cleaned_pr_data", 
-                  "json")
+    cleaned_pr_data_file_path = write_to_file(cleaned_pr_data, 
+                                              config["CLEANED_DATA_PATH"], 
+                                              "cleaned_pr_data", 
+                                              "json")
+    return cleaned_pr_data_file_path
 
 def clean_pr_comments_data(config, pr_comments_file):
     pr_comments_data = read_file(pr_comments_file)
@@ -43,20 +42,18 @@ def clean_pr_comments_data(config, pr_comments_file):
     cleaned_pr_comments_data = []
     for pr_comment in pr_comments_data:
         cleaned_pr_comments_data.append({
-            "PR Comment URL": pr_comment["html_url"],
-            "PR URL": pr_comment["html_url"].split("#issuecomment")[0],
-            "PR Comment User": pr_comment["user"]["login"],
-            "PR Comment User Type": pr_comment["user"]["type"],
-            "PR Comment Created At": pr_comment["created_at"],
-            "PR Comment Updated At": pr_comment["updated_at"],
-            "PR Comment Author Association": pr_comment["author_association"],
-            "PR Comment Body": pr_comment["body"]
+            "PR Comment URL": pr_comment.get("html_url", None),
+            "PR URL": pr_comment.get("html_url", None).split("#issuecomment")[0],
+            "PR Comment User": pr_comment.get("user", {}).get("login", None),
+            "PR Comment User Type": pr_comment.get("user", {}).get("type", None),
+            "PR Comment Created At": pr_comment.get("created_at", None),
+            "PR Comment Updated At": pr_comment.get("updated_at", None),
+            "PR Comment Author Association": pr_comment.get("author_association", None),
+            "PR Comment Body": pr_comment.get("body", None)
         }) 
     
-    write_to_file(cleaned_pr_comments_data, 
-                  config["CLEANED_DATA_PATH"], 
-                  "cleaned_pr_comments_data", 
-                  "json")
-
-clean_pr_data(config, "./raw_data/pr_data_20230918_132543.json")
-# clean_pr_comments_data(config, "./raw_data/comments_data_20230918_151849.json")
+    cleaned_pr_comment_data_file_path = write_to_file(cleaned_pr_comments_data, 
+                                              config["CLEANED_DATA_PATH"], 
+                                              "cleaned_pr_comments_data", 
+                                              "json")
+    return cleaned_pr_comment_data_file_path
